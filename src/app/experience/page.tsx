6ef34b5070a3d302d5996078c96637b90c2c5271
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-08-18 18:50:13 -0300

    feat: add image in view experience

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -5,7 +5,7 @@ export default function ProjectPage() {
         Experiências.
       </h1>
 
-      <div className="flex flex-col items-start justify-center gap-16 h-2/3 max-sm:h-full relative w-full">
+      <div className="flex flex-col items-start justify-center gap-16 h-3/4 max-sm:h-full relative w-full">
         <div className="border-l-8 border-l-zinc-700 rounded-tl-md rounded-bl-md flex flex-col gap-3 pl-3">
           <h2 className="text-lg font-semibold">QA Junior 3</h2>
           <span className="text-sm flex items-center gap-2">
@@ -64,7 +64,7 @@ export default function ProjectPage() {
         <img
           src="/dots.png"
           alt="points"
-          className="absolute h-[600px] w-[600px] opacity-40 right-0 animate-spin animate-duration-[8000ms]"
+          className="absolute h-[600px] w-[600px] opacity-40 right-0 animate-spin animate-duration-[12000ms]"
         />
       </div>
     </section>
