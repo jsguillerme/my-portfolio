1b032f9ba45b5b5603f3b123eb08a2fef87922a0
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-08-18 20:40:39 -0300

    fix: adjusts in height header component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,4 @@
 export default function Loading() {
-  // Or a custom loading skeleton component
-  // eslint-disable-next-line react/no-unescaped-entities
   return (
     <div className="w-full h-full flex items-center justify-center text-white font-sans font-bold">
       <p className="animated-text text-center text-9xl">G</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,31 @@
+import Link from 'next/link'
+
 export function Header() {
   return (
-    <div className="max-w-[1124px] mx-auto w-full h-32 max-h-32 px-8 max-sm:p-4 flex items-center justify-between max-sm:h-fit max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:gap-4">
-      <a href="/" title="Voltar ao início">
+    <div className="max-w-[1124px] mx-auto w-full min-h-[128px] max-h-32 px-8 max-sm:p-4 flex items-center justify-between max-sm:h-fit max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:gap-4">
+      <Link href="/" title="Voltar ao início">
         <img src="/logo-gradient.png" width={32} height={32} alt="" />
-      </a>
+      </Link>
 
       <div className="flex items-center justify-between gap-4 dark:text-neutral-400 text-zinc-900 max-sm:w-full font-light font-mono">
-        <a
+        <Link
           href="/about"
           className="hover:text-neutral-200 transition-all duration-200 w-32 dark:hover:bg-zinc-700/50 py-2 text-center rounded-md"
         >
           Sobre
-        </a>
-        <a
+        </Link>
+        <Link
           href="/projects"
           className="hover:text-neutral-200 transition-all duration-200 w-32 dark:hover:bg-zinc-700/50 py-2 text-center rounded-md"
         >
           Projetos
-        </a>
-        <a
+        </Link>
+        <Link
           href="/experience"
           className="hover:text-neutral-200 transition-all duration-200 w-32 dark:hover:bg-zinc-700/50 py-2 text-center rounded-md"
         >
           Experiência
-        </a>
+        </Link>
       </div>
 
       <button className="dark:text-neutral-400 dark:hover:text-neutral-200 transition-all duration-150 hover:scale-125 text-4xl max-sm:hidden">
